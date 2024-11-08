7362029fbb4de6cd996c09095849368b877ddcb3
Alex-maslo <4439897@gmail.com>
2024-11-08 16:00:09 +0200

    [additional]:work commit

---
+++ script-add-1.js
@@ -0,0 +1,19 @@
+// #WpkK0ZH1
+// --створити масив з:
+// - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+// Масив з 5 числових значень
+let numberArray = [1, 2, 3, 4, 5];
+
+// Масив з 5 рядкових значень
+let stringArray = ["one", "two", "three", "four", "five"];
+
+// Масив з 5 значень різних типів: стрічковий, числовий, булевий
+let mixedArray = ["text", 10, true, "example", false];
+
+console.log(numberArray);
+console.log(stringArray);
+console.log(mixedArray);
