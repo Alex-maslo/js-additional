5f627c8485e37d827979ba742ec6a2e57bdf85d2
Olexandr <4439897@gmail.com>
2024-11-09 13:26:22 +0200

    [additional]:home commit

---
+++ additional-1/script-add-4.js
@@ -0,0 +1,7 @@
+// #mDMWMW5a
+// - Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+
+for (let i = 0; i < 100; i += 2) {
+  console.log(i);
+  document.write(`<h3>${i}</h3>`);
+}
